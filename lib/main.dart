c59922952df3207d0dfdd2dec81536e4d7dfe075
Pavaneeshwar <pavaneeshwar7077@gmail.com>
2022-07-14 22:15:59 +0530

    14-july

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "instagram clone",
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello world",style: TextStyle(fontSize: 24),),
+        ),
+      ),
+    );
+  }
+}
