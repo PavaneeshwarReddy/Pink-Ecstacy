1d8263a10117fefed5afaf7868d1d5817e1e30e4
Pavaneeshwar <pavaneeshwar7077@gmail.com>
2022-07-16 13:26:58 +0530

    colors.dart

---
+++ lib/utitlities/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const bubble_blue = Color.fromRGBO(212, 242, 255, 1);
+const bubble_pink = Color.fromRGBO(255, 223, 254, 1);
+const bubble_red = Color.fromRGBO(2255, 175, 175, 1);
+const bubble_green = Colors.blue;
+const text_color_light = Color.fromRGBO(28, 28, 28, 0.75);
+const text_color_medium_dark = Color.fromRGBO(28, 28, 28, 0.62);
+const text_color_dark = Color.fromRGBO(28, 28, 28, 0.9);
